e4c4d2bf0724be1163272b64c304aceb2770fb59
Padmanabhuni Venkata Saiteja <saitejapadmanabhunivenkata@PadmanahunisMBP.Dlink>
2024-03-26 18:15:16 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:mybmi/screens/homescreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          color: Color(0xFF00BB6E),
+        ),
+        primaryColor: Colors.white,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: Colors.green,
+        ),
+      ),
+      title: 'Flutter Demo',
+      home: const HomeScreen(),
+    );
+  }
+}
